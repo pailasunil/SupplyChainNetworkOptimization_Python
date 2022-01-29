032728f9526757c3ec49e34e3b8bc90b8fa86148
pailasunil <77604246+pailasunil@users.noreply.github.com>
2022-01-29 23:48:17 +0530

    Add files via upload

---
+++ test.py
@@ -0,0 +1,8 @@
+# My first Streamlit App"
+
+# Load Streamlit library
+import streamlit as st
+
+# Hello World
+st.title('Hello World')
+st.write("\nThis is my first Web App, built using Streamlit!")
